e24d8106008649f87fd0d80634e94ddee467afc8
r7chel <rachellroodd@gmail.com>
2021-02-16 11:29:17 -0800

    swaps

--- student.c
+++ student.c
@@ -1,17 +1,34 @@
 #include "student.h"
 
 int largest(int array[], int length) {
+  //loop through length of array
+  for (int i = 0; i < length, i++) {
+
+  }
   return -1;
 }
 
 int sum(int array[], int length) {
+    //loop through length of array
+  for (int i = 0; i < length, i++){
+  } 
   return -1;
 }
 
 void swap(int *a, int *b) {
+  //swap for 2 vars
+  int t = *a;
+  *a = *b;
+  *b = t;
 }
 
 void rotate(int *a, int *b, int *c) {
+  //swap for 3 vars
+  int t = *a;
+  int t2 = *b;
+  *a = *c;
+  *b = t;
+  *c = t2;
 }
 
 void sort(int array[], int length) {
@@ -21,4 +38,5 @@ void double_primes(int array[], int length) {
 }
 
 void negate_armstrongs(int array[], int length) {
+
 }
