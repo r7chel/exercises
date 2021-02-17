8d6a424eed196c94e204b7c239c494a708ffc637
r7chel <rachellroodd@gmail.com>
2021-02-16 16:54:31 -0800

    6/10.2

--- student.c
+++ student.c
@@ -50,13 +50,42 @@ void sort(int array[], int length) {
 
 void double_primes(int array[], int length) {
       //loop through length of array
-  for (int i = 0; i < length; i++){
+  for (int i = 0; i < length + 1; i++){
+    int notprime = 0;
+    for (int j = 2; j < i/2; j++){
+      if (i % j == 0){
+        notprime = 1;
+        break;
+      }
+    }
 
+      if (notprime==0 && i > 1 && array[i] >= 0) {
+
+        array[i] = array[i]*2;
+    }
+  
   }
+  
 }
 void negate_armstrongs(int array[], int length) {
         //loop through length of array
+  int total = 0;
   for (int i = 0; i < length; i++){
+    //number of digits in array[i]
+    int len = 0;
+    //temps for array[i]
+    int t = array[i];   
+    int t2 = array[i];
+    while (t){
+      t /= 10;
+      len += 1;
+    }
+    while (t2>0){
+    int digit = t2 % 10;
+   // total += digit ** len;
+    t2 /= 10;
+    }
+  
 
-}
+  }
 }
